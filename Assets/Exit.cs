99661f8868149becc611051d465a1bc53be913cd
matthew <matthew_p_thomas@internode.on.net>
2021-02-10 11:10:47 +1030

    imported maze

---
+++ Assets/Exit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("coliddd");
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("You Won!");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("triggered");
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(Vector2 movementValue)
+    {
+        Vector2 movementVector = movementValue;
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "PickUp") {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
