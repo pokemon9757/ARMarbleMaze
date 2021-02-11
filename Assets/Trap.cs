3af51ad31267406c85e344f472f5c0b620f44638
Xuan Tien Pham <phaxy003@mymail.unisa.edu.au>
2021-02-11 17:02:49 +1030

    Modified maze

---
+++ Assets/Trap.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other) {
+        if(other.collider.tag == "Player"){
+            other.collider.gameObject.SetActive(false);
+        }
+    }
+}
